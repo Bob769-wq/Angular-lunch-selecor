9e1fdeca500e0c4f5db2b7deaa6e8ea4c697e29a
Bob <dash78231@gmail.com>
2025-06-09 17:14:14 +0800

    Todo App first step

---
+++ src/app/todo-app/todo-app.component.ts
@@ -0,0 +1,16 @@
+import { Component, computed } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Expression } from "@angular/compiler";
+
+
+@Component ({
+    selector:'app-todo-app',
+    standalone:true,
+    imports:[CommonModule],
+    template:``,
+    styles:[]
+})
+
+export class TodoAppComponent {
+    
+}
